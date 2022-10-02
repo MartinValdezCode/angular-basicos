e0aac2231a4097c9ffe01ce63d7b236c936eb5b0
Martín Valdez <valdez.martin.code@gmail.com>
2022-10-02 05:23:26 -0300

    Finalizada sección 5

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
+import { PersonajesComponent } from '../personajes/personajes.component';
+
+@Injectable()
+export class DbzService {
+
+    private _personajes: Personaje[] = [
+        {
+            nombre: 'Goku',
+            poder: 15000
+        },
+        {
+            nombre: 'Vegeta',
+            poder: 7500
+        }
+    ];
+
+    get personajes(): Personaje[] {
+        return [...this._personajes];
+    }
+
+    constructor() { }
+
+    agregarPersonaje(personaje: Personaje): void {
+        this._personajes.push(personaje);
+    }
+}
